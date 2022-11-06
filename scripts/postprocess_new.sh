7f33f594721fef95e44205d4223f3eef8f3e4850
Elias Schuhmacher <eliassebastian.schuhmacher@uzh.ch>
2022-11-06 22:45:16 +0100

    add second improvement strategy and cleanups

--- scripts/postprocess_new.sh
+++ scripts/postprocess_new.sh
@@ -2,4 +2,5 @@ infile=$1
 outfile=$2
 lang=$3
 
+## Add command to decode BPE: sed -r 's/(@@ )|(@@ ?$)//g'
 cat $infile | sed -r 's/(@@ )|(@@ ?$)//g' | perl moses_scripts/detruecase.perl | perl moses_scripts/detokenizer.perl -q -l $lang > $outfile
